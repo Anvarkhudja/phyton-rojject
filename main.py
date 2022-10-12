2edbebf85fdad1ddc7216567d3819461ccc26636
unknown <anvarkaiumkhodzhaev@gmail.com>
2022-10-12 18:54:06 +0500

    fiest element

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def hello():
-    print("hello world")
-
-
-hello()
-a = 5
-print(a)
+a = [1, 2, 3, 4, 6, 7, 99, 88, 999]
+max_num = 0
+for i in a:
+    if i > max_num:
+        max_num = i
+print("Eng katta son: ", max_num)
