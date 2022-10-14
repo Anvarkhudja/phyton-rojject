a5ee979ffd9de6b07abd7524cbdacb957d4112bd
islomabdullaev <islom.abdullaev.02@mail.ru>
2022-10-14 19:28:22 +0500

    hacked

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 a = [1, 2, 3, 4, 6, 7, 99, 88, 999]
 max_num = 0
 for i in a:
-        sif i > max_num:
-            max_num = i
+    if i > max_num:
+        max_num = isssss
 print("Eng katta son: ", max_num)
-
-# Salom Anvarho'ja
-# Hello
