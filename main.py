9123095013d97bc6e3a1494a508a9e00cca2a26d
unknown <anvarkaiumkhodzhaev@gmail.com>
2022-10-14 17:51:29 +0500

    g

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-a = [1, 2, 3, 4, 6, 7, 99, 88, 1000]
+a = [1, 2, 3, 4, 6, 7, 99, 88, 100]
 max_num = 0
 for i in a:
     if i > max_num:
         max_num = i
 print("Eng katta son: ", max_num)
+# 1000 qilingan
